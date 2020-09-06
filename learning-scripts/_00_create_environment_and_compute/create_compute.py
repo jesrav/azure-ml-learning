7344ec9b02f9b5b31ab5ce83eda0a325363d4896
Ravnbøl Jes <jerav@DFDS.COM>
2020-09-06 12:53:34 +0200

    Create compute cluster

--- learning-scripts/_00_create_environment_and_compute/create_compute.py
+++ learning-scripts/_00_create_environment_and_compute/create_compute.py
@@ -1,3 +1,16 @@
 from azureml.core import Workspace
+from azureml.core.compute import ComputeTarget, AmlCompute
+from azureml.core.compute_target import ComputeTargetException
 
 ws = Workspace.from_config()
+
+compute_name = "aml-cluster"
+
+try:
+    aml_cluster = ComputeTarget(workspace=ws, name=compute_name)
+    print("Found existing cluster.")
+except ComputeTargetException:
+    compute_config = AmlCompute.provisioning_configuration(
+        vm_size="STANDARD_DS12_V2", max_nodes=4
+    )
+    aml_cluster = ComputeTarget.create(ws, compute_name, compute_config)
